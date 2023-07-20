597c4963368a9b0938f55614c320c67c0b508d00
Marijn Schouten <mas@deep-ink.ventures>
2023-07-20 14:44:00 +0200

    WIP procedural macro

---
+++ pallets/hookpoints/macros/tests/test.rs
@@ -0,0 +1,10 @@
+use macros::hooks;
+
+struct AccountId;
+struct DispatchError;
+
+#[hooks]
+trait GenesisDAO {
+    fn voting_power(&self, voter: AccountId, balance: u128) -> u128;
+	// ... more fn's
+}
